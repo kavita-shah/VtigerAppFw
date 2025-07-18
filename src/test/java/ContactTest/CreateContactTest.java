c8e2f19a820923ba1bc3f8cd16d0ca73f2cfe133
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-18 17:11:20 +0530

    Repoitory is created

---
+++ src/test/java/ContactTest/CreateContactTest.java
@@ -0,0 +1,11 @@
+package ContactTest;
+
+public class CreateContactTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("createContactPage");
+
+	}
+
+}
