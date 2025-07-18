610f7723519093f134b51c070049f4f20ec90c1c
kavita-shah <https://github.com/kavita-shah/VtigerAppFw.git>
2025-07-18 18:13:19 +0530

    change code

--- src/test/java/ContactTest/CreateContactTest.java
+++ src/test/java/ContactTest/CreateContactTest.java
@@ -5,6 +5,7 @@ public class CreateContactTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("createContactPage");
+		System.out.println("CreateOrgPage");
 
 	}
 
